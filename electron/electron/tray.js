00fa0c5f01de82a0e0cb5ac99f83b93acc51f9c1
ITSM <tinyminer2015@gmail.com>
2026-06-07 00:50:00 +0100

    feat(app): tray, keep-alive, launch-at-login

---
+++ electron/electron/tray.js
@@ -0,0 +1,17 @@
+const { Tray, Menu, nativeImage } = require("electron");
+const path = require("path");
+
+function createTray({ onShow, onQuit }) {
+  const icon = nativeImage.createFromPath(path.join(__dirname, "..", "build", "icon.png"));
+  const tray = new Tray(icon.isEmpty() ? nativeImage.createEmpty() : icon);
+  tray.setToolTip("Ableton Backup");
+  tray.setContextMenu(Menu.buildFromTemplate([
+    { label: "Show", click: onShow },
+    { type: "separator" },
+    { label: "Quit", click: onQuit },
+  ]));
+  tray.on("click", onShow);
+  return tray;
+}
+
+module.exports = { createTray };
